a5e1fd1bfcc71f4cdb493ee24eedff6c2b254ea0
SzymKam <szymon15kaminski@gmail.com>
2024-01-25 13:26:42 +0100

    feat: add exercise

---
+++ ex_2.py
@@ -0,0 +1,23 @@
+"""
+Given an integer n and two other values, build an array of size n filled with these two values alternating.
+
+Examples
+5, true, false     -->  [true, false, true, false, true]
+10, "blue", "red"  -->  ["blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red"]
+0, "one", "two"    -->  []
+"""
+
+
+def arr(number, value_1, value_2):
+    list = []
+    for num in range(number):
+        if num % 2 == 0:
+            list.append(value_1)
+        else:
+            list.append(value_2)
+    return list
+
+
+assert arr(5, True, False) ==  [True, False, True, False, True]
+assert arr(10, "blue", "red") ==  ["blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red"]
+assert arr(0, "one", "two") == []
