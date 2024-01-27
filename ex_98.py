1e82dfdb8d0cca77eddbea85211ec71ff9520aaa
SzymKam <szymon15kaminski@gmail.com>
2024-01-27 22:45:02 +0100

    feat: add exercise

---
+++ ex_98.py
@@ -0,0 +1,9 @@
+def zewnetrzna_funkcja(x):
+    def wewnetrzna_funkcja(y):
+        return x + y
+    return wewnetrzna_funkcja
+
+closure = zewnetrzna_funkcja(10)
+wynik = closure(5)
+
+print(wynik)
