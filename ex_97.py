bdfa4d2812690f70f01f106c1794cc893cb1fa5d
SzymKam <szymon15kaminski@gmail.com>
2024-01-27 22:44:52 +0100

    feat: add exercise

---
+++ ex_97.py
@@ -0,0 +1,31 @@
+# import asyncio
+#
+# async def wspolprogram():
+#     print("Początek współprogramu")
+#     await asyncio.sleep(2)  # Symulacja operacji asynchronicznej
+#     print("Koniec współprogramu")
+#
+# # Utwórz pętlę asynchroniczną
+# async def main():
+#     print("Rozpoczęcie programu")
+#     await wspolprogram()
+#     print("Zakończenie programu")
+#
+# # Uruchom pętlę asynchroniczną
+# asyncio.run(main())
+from time import sleep
+
+
+def wspolprogram():
+    print("Początek współprogramu")
+    sleep(2)  # Symulacja operacji asynchronicznej
+    print("Koniec współprogramu")
+
+# Utwórz pętlę asynchroniczną
+def main():
+    print("Rozpoczęcie programu")
+    wspolprogram()
+    print("Zakończenie programu")
+
+# Uruchom pętlę asynchroniczną
+main()
