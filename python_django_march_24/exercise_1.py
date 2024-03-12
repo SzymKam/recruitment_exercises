a2f842c5a1d1d894ff9fb25139ff373608aefa39
SzymKam <szymon15kaminski@gmail.com>
2024-03-12 18:04:20 +0100

    feat: update exercise

---
+++ python_django_march_24/exercise_1.py
@@ -0,0 +1,7 @@
+"""1. Reverse the list without using builtin functions
+odd_numbers = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+"""
+
+odd_numbers = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+even_numbers = odd_numbers[::-1]
+
