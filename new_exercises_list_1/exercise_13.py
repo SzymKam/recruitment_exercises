beef160b4f5a55955386ff6105334f05fc77a358
SzymKam <szymon15kaminski@gmail.com>
2024-03-14 17:58:44 +0100

    feat: add exercise

---
+++ new_exercises_list_1/exercise_13.py
@@ -0,0 +1,12 @@
+x = (
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit." 
+"Sed euismod arcu non quam dignissim, sed suscipit sem dictum." 
+"Morbi non vulputate orci. Donec ut auctor elit."
+)
+
+print(type(x))
+
+"x type is 'str'"
+
+y = ('xxxx''yyy''zzz')
+print([i for i in y])
