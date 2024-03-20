3ecf2d98e9af785c4f7fbb43c7c0fa4db44355c4
SzymKam <szymon15kaminski@gmail.com>
2024-03-20 10:40:37 +0100

    feat: add exercise

---
+++ python_questions/ex_2.py
@@ -0,0 +1,15 @@
+"""
+Zad. 2
+Jaś chciałby skomunikować się z Małgosią, używając protokołu HTTPS. Niestety istnieje czarownica,
+która chciałaby podsłuchać informacje wysyłane przez Jasia i wysłać podmienioną wiadomość do  Małgosi.
+Który z poniższych kroków ataku Man in the Middle jest niepoprawny?
+
+A. Jaś prosi Małgosię o klucz publiczny
+B. Małgosia wysyła swój publiczny klucz do Jasia
+C. Czarownica przechwytuje publiczny klucz wysłany przez Małgosię i wysyła swój prywatny klucz do  Jasia
+D. Jaś koduje wiadomość kluczem czarownicy myśląc, że jest to klucz Małgosi i wysyła wiadomość do  Małgosi
+E. Czarownica przechwytuje wiadomość od Jasia, rozkodowuje wiadomość i koduje kluczem Małgosi  podmienioną wiadomość i wysyła ją do Małgosi
+
+"""
+
+"C - wysyła klucz publiczny"
