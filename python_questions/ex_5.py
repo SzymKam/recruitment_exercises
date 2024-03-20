d602e837f8c45fb695881941edcfbbc9f89daeb8
SzymKam <szymon15kaminski@gmail.com>
2024-03-20 10:40:51 +0100

    feat: add exercise

---
+++ python_questions/ex_5.py
@@ -0,0 +1,12 @@
+"""
+Zad. 5
+Załóżmy że masz kartkę papieru, którą składasz wiele razy na pół.
+Każda taka akcja dzieli kartkę na  coraz większą liczbę „prostokątów”.
+Jaka jest złożoność obliczeniowa podziału kartki na n części w  funkcji obliczeń złożeń?
+      A. O(n^2)
+    B. O(n)
+    C. O(logn)
+    D. O(1)
+"""
+
+"A - zależność kwadratowa - 10 złożeń da 1024 warstwy"
