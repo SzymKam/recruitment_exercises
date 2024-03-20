c51455624ab197ebca3353628ef293be5a5c68ea
SzymKam <szymon15kaminski@gmail.com>
2024-03-20 10:40:41 +0100

    feat: add exercise

---
+++ python_questions/ex_3.py
@@ -0,0 +1,11 @@
+"""
+Zad. 3
+Załóżmy, że masz dwie struktury danych - stos i kolejkę - do których dodałeś kilka elementów.
+W  której z nich szybciej dostaniesz się do elementu, który dodałeś jako pierwszy?
+
+A. Stos
+B. Kolejka
+
+"""
+
+"A - kolejka, ponieważ bierze elementy jeden po drugim od początku. Stos układa jeden na drugi - pierwszy jest na dole"
