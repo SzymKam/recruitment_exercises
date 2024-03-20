71475db33b017ebc1b30c54ebc57dab104b47d32
SzymKam <szymon15kaminski@gmail.com>
2024-03-20 10:40:43 +0100

    feat: add exercise

---
+++ python_questions/ex_4.py
@@ -0,0 +1,16 @@
+"""
+Zad. 4
+Załóżmy, że masz następującą tablicę liczb: [1, 2, 3, 4, 5, 6].
+Chcesz zsumować jej elementy,  wykorzystując do tego prosty algorytm rekurencyjny.
+Przyjmijmy, że wywołanie metody obliczającej  sumę wygląda następująco: sum([1, 2, 3, 4, 5, 6]).
+Jaki mógłby być warunek zakończenia rekurencji w  takim przypadku.
+
+
+A. sprawdzenie, że długość tablicy = 1
+B. sprawdzenie, że przetwarzany element tablicy ma wartość 1
+C. sprawdzenie, że licznik wywołań metody sum = (długość tablicy / 2)
+D. żadna z powyższych odpowiedzi nie jest poprawna
+
+"""
+
+'D - zadna nie jest wlaściwa'
