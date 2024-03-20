f8de3a03716f670a24fb3747e3a77385de07d156
SzymKam <szymon15kaminski@gmail.com>
2024-03-20 13:42:01 +0100

    feat: add exercise

---
+++ python_questions/ex_7.py
@@ -0,0 +1,13 @@
+"""
+Zad. 7
+Załóżmy, że mamy tablicę skrótów, pozwalającą na przechowanie 7 elementów oraz funkcję  obliczania skrótów zwracającą
+jako indeks wynik z dzielenia modulo 7. Która z tablic zawiera  poprawny rozkład liczb: 5, 7, 58, 73, 13, 55?"""
+"modulo to reszta z dzielenia"
+'% 7'
+"5 -> 5"
+"7 -> 0"
+"58 -> 2"
+"73 -> 3"
+"13 -> 6"
+"55 -> 6"
+"Odpowiedź B"
