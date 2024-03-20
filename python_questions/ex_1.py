7bc8a703892559be700728ab12709e16b392b5fe
SzymKam <szymon15kaminski@gmail.com>
2024-03-20 10:40:25 +0100

    feat: add exercise

---
+++ python_questions/ex_1.py
@@ -0,0 +1,12 @@
+"""Aplikacja wykonuje zapytania typu POST, DELETE i PUT do obsługi backendowej.
+Wszystkie zapytania  zwróciły kod błędu 500.
+Którą z nich zgodnie ze specyfikacją metod http, nie można bezpiecznie  ponowić?
+
+A. POST
+B. DELETE
+C. PUT
+D. wszystkie można ponowić
+"""
+
+"500 Internal Server Error"
+"Można ponowić każde z zapytań"
