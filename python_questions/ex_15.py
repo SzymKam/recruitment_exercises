2cab92aed918fdfb3161d3a4530d1e0b02333db2
SzymKam <szymon15kaminski@gmail.com>
2024-03-21 23:17:15 +0100

    feat: add exercise

---
+++ python_questions/ex_15.py
@@ -0,0 +1,13 @@
+"""
+Załóżmy, że mamy dwa problemy decyzyjne:
+1. Jesteś na zakupach i chcesz sprawdzić, czy wszystkie produkty, które masz w koszyku,
+znajdują się na liście którą wcześniej przygotowałeś. Lista zawiera n produktów, w koszyku  masz k produktów.
+2. Chcesz otworzyć sejf, ale zapomniałeś kodu dostępu do zamka elektronicznego.
+Metodą prób i błędów starasz się znaleźć poprawny n-cyfrowy kod.
+Która odpowiedź jest poprawna?
+       A. Problem 1 jest klasy P, problem 2 jest klasy NP.
+ B. Problem 1 jest klasy NP, problem 2 jest klasy P
+ C. Oba problemy są klasy P
+ D. Problem 2 jest zarówno klasy P jak i NP.
+
+"""
