99a19cbffab7beee3fcc89dd3195766a6d4925b0
SzymKam <szymon15kaminski@gmail.com>
2024-03-21 23:16:56 +0100

    feat: add exercise

---
+++ python_questions/ex_11.py
@@ -0,0 +1,24 @@
+"""
+Załóżmy że planujesz pojechać w weekend na wycieczkę w góry.
+Masz plecak, w którym zmieścisz  przedmioty o maksymalnej wadze 6 kg.
+Każdy z nich ma dodatkowo przypisany priorytet pozwalający  określić ważność danego przedmiotu.
+Możesz zabrać przedmioty z poniższej listy:
+1. Namiot, 3 kg 10
+2. Czytnik ebooków, 1 kg 3
+3. Kanapki 2 kg, 3
+4. Kurtka 2 kg, 5
+5. Apteczka 1 kg 6
+Znajdź zestaw przedmiotów o jak największej sumie priorytetów, który zmieści się w plecaku i  zaznacz te które powinieneś wziąć.
+
+"""
+
+
+"""
+namiot 1/3.33
+czytnik 1/3
+kanapki 1/1.5
+kurtka 1/2.5
+apteczka 1/6
+
+apteczka + namiot + kurtka
+"""
