a7755fa06643fcac8686412e2435acfaba5fcb0c
SzymKam <szymon15kaminski@gmail.com>
2024-03-21 23:17:08 +0100

    feat: add exercise

---
+++ python_questions/ex_14.py
@@ -0,0 +1,11 @@
+"""
+Zad. 14
+Załóżmy, że chcesz podzielić prostokąt o wymiarach 1920 cm x 1080 cm na równe, jak największe  kwadraty.
+Zad. 14.1
+Jaki jest największy możliwy rozmiar (bok) takiego kwadratu?
+A. 90 cm
+B. 100 cm
+C. 80 cm
+D. 120 cm
+16x9
+"""
