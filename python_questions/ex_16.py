76ef25ba49b40c31bed838d3a750a3a4fc8fb163
SzymKam <szymon15kaminski@gmail.com>
2024-03-22 16:30:16 +0100

    feat: add exercise

---
+++ python_questions/ex_16.py
@@ -0,0 +1,10 @@
+a = ['orange', 'apple', 'banana']
+b = a
+b = ['tomato', 'cucumber', 'carrot']
+
+print(a)
+"""    
+A. [‘tomato’, ‘cucumber’, ‘carrot’] 
+B. [‘orange’, ‘apple’, ‘banana’] - prawidłowa, bo do B przypisujemy nową wartość, nie modyfikując a
+C.  None
+"""
