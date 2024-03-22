133c49fed844f51192e0d4e8e08d985daef1d592
SzymKam <szymon15kaminski@gmail.com>
2024-03-22 16:30:31 +0100

    feat: add exercise

---
+++ python_questions/ex_20.py
@@ -0,0 +1,18 @@
+"""
+Zad. 20
+Co jest nie tak z poniższym kodem?
+
+MULTIPLIER = 8
+a = lambda x,y: (x*MULTIPLIER)  / y
+print(a(2, 3))
+
+
+A. Funkcja lambda nie może zostać przypisana do zmiennej
+B. Lambda nie ma wyrażenia return
+C. Lambda może przyjąć tylko jeden argument
+D. Nie ma błędu w powyższym wyrażeniu
+
+"""
+
+"odpowiedź D"
+
