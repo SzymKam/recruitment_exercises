0ee81b866f41edc1473ae19fe58f8d3b7d140db8
SzymKam <szymon15kaminski@gmail.com>
2024-03-22 16:30:19 +0100

    feat: add exercise

---
+++ python_questions/ex_17.py
@@ -0,0 +1,13 @@
+"""
+Które zdanie odnosi się do wyrażeń i obiektów możliwych do wykorzystania w wyrażeniu ‘with’:
+
+with expression (as var):
+  ...BODY...
+
+A. metoda __exit__() zostanie wywołana tylko wtedy, gdy BODY napotka wyjątek
+B. Wynikiem ‘expression’ musi być Context Manager (obiekt, który implementuje, tzw. Context Management protocols)
+C. Obiekt, który jest wynikiem expression musi posiadać takie metody jak: __enter__() oraz __exit__()
+
+"""
+'B oraz C?'
+
