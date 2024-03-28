d12f7a0613f91d9b76b50ffc5db1860d2c17e46d
SzymKam <szymon15kaminski@gmail.com>
2024-03-28 23:14:44 +0100

    fix: update exercise

--- ex_2.py
+++ ex_2.py
@@ -1,6 +1,5 @@
 """
 Given an integer n and two other values, build an array of size n filled with these two values alternating.
-
 Examples
 5, true, false     -->  [true, false, true, false, true]
 10, "blue", "red"  -->  ["blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red"]
