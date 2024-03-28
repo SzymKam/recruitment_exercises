269e7b4aa8c9f165e5a2517c4acfcc038e18d46f
SzymKam <szymon15kaminski@gmail.com>
2024-03-28 23:15:19 +0100

    fix: update exercise

--- ex_99.py
+++ ex_99.py
@@ -6,8 +6,6 @@ with open('plik.pkl', 'wb') as file:
     pickle.dump(data, file)
 
 
-
-
 # Deserializacja za pomocą Pickle
 with open('plik.pkl', 'rb') as file:
     restored_data = pickle.load(file)
