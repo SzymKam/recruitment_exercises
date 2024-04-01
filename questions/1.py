11234e54eba12199ee23b3817f28e29db2514994
SzymKam <szymon15kaminski@gmail.com>
2024-04-01 23:19:22 +0200

    fix: update exercise

--- questions/1.py
+++ questions/1.py
@@ -5,9 +5,11 @@ Składa się on z siedmiu warstw, każda zajmuje się innym aspektem kompunikacj
 1. Warstwa fizyczna - najniższa warstwa. Odpowiada za przesył surowych danych binarnych
 między urządzeniami. Są to przewody, syngały elekryczne/optyczne/radiowe. Dane pzesyłane w
 formie binarnej.
-2. Warstwa łącza danych - odpowiada ona za kontrolę przeyłanych danych pomiędzy urządzeniami
-(kontrola i nadzór nad warstwą fizyczną).
-Odpowiada za adresowaniu, routowanie, i wybór najlepszej ścieżki dla pakietów danych. Tutaj
+2. Warstwa łącza danych - odpowiada za bezbłędną transmisję danych między dwoma urządzeniami
+(kontrola i nadzór nad warstwą fizyczną). Zapewnia kontrolę błędów - wykrywanie i korekcję.
+Pakowanie danych w ramki.
+3. Warstwa sieciowa - ma wiedzę na temat topologii sieci. Rozpoznaje drogi łączące dwa urządzenia
+i decyduje ile informacji jaką drogą/połączeniem przesłać. Odpowiada za adresowaniu, routowanie, i wybór najlepszej ścieżki dla pakietów danych. Tutaj
 działają protokoły IP.
-3.
+4. Warstwa transportowa -
 """
