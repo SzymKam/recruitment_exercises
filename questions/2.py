26587c17cc4916ef91a0a4052974ae749659c3d7
SzymKam <szymon15kaminski@gmail.com>
2024-04-01 23:45:21 +0200

    fix: update exercise

---
+++ questions/2.py
@@ -0,0 +1,9 @@
+"""Czym się różni Hub od Switcha od Routera?
+urządzenia wykorzystywane w sieciach komputerowych.
+Hub - najprostsze. Urządzenie fizyczne, działa na 1 warstwie modelu OSI. Dane z jednego
+portu przsyła na wszystkie inne porty. Wszystkie urządzenia podłączone do Huba, widzą wszystkie
+dane przesyłane przez inne urządzenia do niego podłączone. Powoduje to duże obciązenie sieci
+oraz ryzyko kolizji.
+
+Switch - urządzenie warstwy 2 modelu OSI - urządzenie dołączania na poziomie łącza danych.
+"""
