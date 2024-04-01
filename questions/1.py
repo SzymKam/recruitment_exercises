d99c0638140345b567650b71b80392d2b612b226
SzymKam <szymon15kaminski@gmail.com>
2024-04-01 23:05:30 +0200

    fix: update exercise

---
+++ questions/1.py
@@ -0,0 +1,6 @@
+"""
+Model OSI (Open Systems Interconnection) to standardowa architektura sieci komputerowych,
+opracowana przez ISO, aby ustandaryzować komunikację pomiędzy różnymi systemami komputerowymi.
+Składa się on z siedmiu warstw, każda zajmuje się innym aspektem kompunikacji.
+1. Warstwa fizyczna -
+"""
