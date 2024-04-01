3154594f69430c783086edb4609ff903b85add9d
SzymKam <szymon15kaminski@gmail.com>
2024-04-01 23:55:46 +0200

    fix: update exercise

---
+++ questions/3.py
@@ -0,0 +1,13 @@
+"""
+Co to jest HTTP? Co to jest HTTPS?
+
+Są to protokoły komunikacyjne wykorzystywane do przesyłania danych między klientem a serwerem.
+
+HTTP - Hypertext Transfer Protocol - podstawowy protokół przeyłu danych, między klienem
+np. przeglądarką a serwerem internetowym. Działa na poziomie aplikacji w modelu OSI - 7
+poziom. Dane są nieszyfrowane - mogą być podatne na ataki.
+
+HTTPS - Hypertext Transfer Protocol Secure - zapewnia dodatkowe wartstwy zabezpieczeń
+poprzez szyfrowanie danych. Wariant HTTP połączony z protokołem SSL/TLS, umożliwia
+bezpieczną komunikację klient - serwer. Dane są szyfrowane. Bezpieczniejsze.
+"""
