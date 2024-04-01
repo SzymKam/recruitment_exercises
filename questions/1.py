07fc18bb6c3c09c2a337d05fd4ea3e8583a4f79b
SzymKam <szymon15kaminski@gmail.com>
2024-04-01 23:14:01 +0200

    fix: update exercise

--- questions/1.py
+++ questions/1.py
@@ -2,5 +2,12 @@
 Model OSI (Open Systems Interconnection) to standardowa architektura sieci komputerowych,
 opracowana przez ISO, aby ustandaryzować komunikację pomiędzy różnymi systemami komputerowymi.
 Składa się on z siedmiu warstw, każda zajmuje się innym aspektem kompunikacji.
-1. Warstwa fizyczna -
+1. Warstwa fizyczna - najniższa warstwa. Odpowiada za przesył surowych danych binarnych
+między urządzeniami. Są to przewody, syngały elekryczne/optyczne/radiowe. Dane pzesyłane w
+formie binarnej.
+2. Warstwa łącza danych - odpowiada ona za kontrolę przeyłanych danych pomiędzy urządzeniami
+(kontrola i nadzór nad warstwą fizyczną).
+Odpowiada za adresowaniu, routowanie, i wybór najlepszej ścieżki dla pakietów danych. Tutaj
+działają protokoły IP.
+3.
 """
