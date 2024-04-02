0b06ea6508ac09d6afcd8acb0cdde943992ad004
SzymKam <szymon15kaminski@gmail.com>
2024-04-02 13:54:18 +0200

    fix: update exercise

---
+++ questions/4.py
@@ -0,0 +1,10 @@
+"""Kiedy użytkownik pobiera stronę internetową przez przeglądarkę,
+jakie porty zostaną użyte po stronie klienta oraz serwera?
+
+Komunikacja odbywa się za pomocą protokołu HTTP lub HTTPS
+SERWER - dla HTTP jest to port 80 a dla HTTPS jest to port 443
+KLIENT - port jest wybierany z puli dostępnych wolnych portów
+
+Wartości te mogą zostać zmieniane przez administratora lub konfigurację serwera.
+
+"""
