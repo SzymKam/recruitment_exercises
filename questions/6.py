887d4278523b48bfaa8c6867e1314eed1476150f
SzymKam <szymon15kaminski@gmail.com>
2024-04-02 14:09:32 +0200

    fix: update exercise

---
+++ questions/6.py
@@ -0,0 +1,6 @@
+"""Co to jest localhost?
+
+Localhost to nazwa hosta, używana do odniesienia się do lokalnego komputera lub urządzenia
+sieciowego. Jest konwencjonalną nazwą dla adresu IP loopback, zwykle ustawiony na:
+127.0.0.1 w IPv4 lub ::1 w IPv6. Używając localhost, odwołujemy się do samego siebie.
+"""
