5adc711ff31febb2d710af4346976af186a035ae
SzymKam <szymon15kaminski@gmail.com>
2024-04-02 14:03:34 +0200

    fix: update exercise

---
+++ questions/5.py
@@ -0,0 +1,7 @@
+"""
+Jak wybrać losowy, wolny port?
+
+Można użyć portu zero. Wtedy zostanie automatycznie przyznany wolny losowy (efemeryczny)
+port z puli aktualnie dostępnych. Nie wszystkie systemy obsługują tę funkcję. Jednak wówczas
+nie mamy kontroli nad tym jaki port został przydzieony. Może to prowadzić do konfliktów.
+"""
