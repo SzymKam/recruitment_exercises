b0bd53a8578ce50c62147841eab3abf2ae2f9fc3
SzymKam <szymon15kaminski@gmail.com>
2024-04-03 23:55:28 +0200

    fix: update exercise

--- questions/8.py
+++ questions/8.py
@@ -1,5 +1,6 @@
 """
 Jakie istnieją podobieństwa pomiędzy "ping" a "traceroute"?
 
-oba z nich służą do diagnostyki i analizy połączeń sieciowych
+oba z nich służą do diagnostyki i analizy połączeń sieciowych. Służą do sprawdzania połączenia
+z innymi hostami w sieci oraz określania trasy jaką przebywa pakiet.
 """
