e61197f582d0cceaf340af1391fde2f358766ba7
SzymKam <szymon15kaminski@gmail.com>
2024-04-03 23:59:15 +0200

    fix: update exercise

--- questions/8.py
+++ questions/8.py
@@ -4,6 +4,9 @@ Jakie istnieją podobieństwa pomiędzy "ping" a "traceroute"?
 oba z nich służą do diagnostyki i analizy połączeń sieciowych. Służą do sprawdzania połączenia
 z innymi hostami w sieci oraz określania trasy jaką przebywa pakiet.
 
+Podobieństwa - testowanie połączena z hostem, diagnozowanie problemów z połączeniem,
+wykorzystanie protokołu ICMP
+
 Traceroute - może zbierać odpowiedzi od hosstów pośredniczących (routers) na trasie pakietu -
 co pozwala na śledzenie trasy którą przechodzi pakiet.
 Ping - prostsze narzedzie, służy głóœwnie do sprawdzania dostępności hosta i
