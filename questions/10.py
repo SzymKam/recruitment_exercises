2020c3140a5642f9a27609025f72663cf4b477f0
SzymKam <szymon15kaminski@gmail.com>
2024-04-04 14:03:34 +0200

    fix: update exercise

---
+++ questions/10.py
@@ -0,0 +1,10 @@
+"""
+Połączenie TCP ma cztery parametry dzieki którym można je jednoznacznie zidentyfikować, co to za parametry?
+
+Parametry te są znane jako "czwórka krotek" - four touple
+
+1. Adres IP zródła: adres IP hosta który inicjuje połączenie
+2. Numer portu źródła: numer portu używany przez aplikację na hoście źródłowym
+3. Adres IP docelowy: adres IP hosta, z którym jest nawiązywane połączenie
+4. Numer portu docelowego hosta: numer portu używanego przez aplikację na hoście docelowym
+"""
