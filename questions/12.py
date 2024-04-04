bc0a453eb711e87869029d9a5714e8beb857e86e
SzymKam <szymon15kaminski@gmail.com>
2024-04-04 14:30:31 +0200

    fix: update exercise

--- questions/12.py
+++ questions/12.py
@@ -1,5 +1,7 @@
 """
 Jakie komendy pokażą tablice routingu na maszynie linuksowej?
 
-
+route -n - starsze polecenie, jeszcze dostępne w niektórych wersjach Linux
+netstat -rn - dostarcza info od podsystemie sieciowym w Linux
+ip route list - nowe polecenie, wyświetla talibe routingu w czytelnej formie
 """
