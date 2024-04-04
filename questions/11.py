8a093c012497a855a687cc04f5509b41761b2efc
SzymKam <szymon15kaminski@gmail.com>
2024-04-04 14:11:45 +0200

    fix: update exercise

---
+++ questions/11.py
@@ -0,0 +1,7 @@
+"""
+Jaka komenda pokaże wszystkie otwarte porty i połączenia na maszynie linuksowej?
+
+netstat - wyświetla informacje o sieci, listy otwartych portów i aktywnych połączeń
+ss - bardziej szczegółowe info o gniazdach sieciowych - podobne do netstat
+lsof - wyświetla info o otwartych plikach i połączeniach w tym połączeniach sieciowych
+"""
