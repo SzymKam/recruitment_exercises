42161b02634fe4a3eeccdbe2dadeb8acde1db2df
SzymKam <szymon15kaminski@gmail.com>
2024-04-04 14:52:02 +0200

    fix: update exercise

---
+++ questions/14.py
@@ -0,0 +1,12 @@
+"""
+Co to jest VLAN?
+
+VLAN - Virtual Local Area Network - jest to logicznie połączona grupa urządzeń, które ze sobą powiązane
+tak jakby były wewnątrz jednej fizycznej sieci lokalnej. Chociaż mogą być fizycznie rozproszone po różnych
+segmentach sieci. VLAN umożliwia segmentację sieci na logiczne grupy.
+
+ - segmentacja sieci - podział fizucznej sieci na wiele wewnątrz tej samej infrastruktury fizycznej,
+ można ograniczyć ruch tylko do urządzeń VLAN.
+ - izolacja ruchu
+ - komunikacja tak jakby były połączone ze sobą fizycznie
+"""
