73dc79f976b66c1fe89433570b1cf09dc15ddb86
SzymKam <szymon15kaminski@gmail.com>
2024-04-05 18:02:55 +0200

    feat: update exercise

---
+++ questions/18.py
@@ -0,0 +1,12 @@
+"""
+co to dhcp
+
+DHCP - to skrót od Dynamic Host Configuration Protocol
+Jest to protokół komunikacyjny służący do dynamicznego przydzielania adresów IP i innych parametrów konfiguracyjnych
+(maska podsieci, bramka domyślna, serwery DNS, serwery czasu) klientom sieciowym. \
+Głównym celem jest DHCP jest ułatwienie i automatyzacja procesu konfiguracji adresów IP
+i innych ustawień dla urządzeń podłączonych do sieci. Adresy IP mogą być przydzielane na dany czas, co oznacza
+potrzebę ich odnowienia po zadanym czasie - dzięki temu lepsze zarządzanie pulą adresów. Umożliwia centralne
+zarządzanie konfiguracją sieci, bo wszystkie parametry sieciową są przyznawane i kontrolowane przez DHCP.
+
+"""
