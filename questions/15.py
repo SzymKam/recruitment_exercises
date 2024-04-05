83c04db6d88f5e7685fd19a492af7aca7dbf1c18
SzymKam <szymon15kaminski@gmail.com>
2024-04-05 18:01:42 +0200

    feat: update exercise

--- questions/15.py
+++ questions/15.py
@@ -9,11 +9,11 @@ w tej samej sieci.
 do wszystkich hostów (broadcastowanie) w tej samej sieci.
 - Zapytanie zawiera adres logiczny hosta docelowego oraz adres fizyczny
 hosta wysyłąjącego zapytanie
-# - na zapytanie odpowiada tylko ten host, którego adres logiczny jest
-# identyczny z adresem logicznym w zapytaniu
-# - odpwiedź zawiera adres logiczny i fizyczny hosta docelowego
-# - odebrany adres fizyczny zapisywany jest w tablicy ARP i parowany z
-# adresem logicznym hosta docelowego, dzięki czemu nie będzie
-# wymagane ponowne odkrywanie adresu fizycznego do momentu wyczyszczenia
-# tablicy
-# """
+- na zapytanie odpowiada tylko ten host, którego adres logiczny jest
+identyczny z adresem logicznym w zapytaniu
+- odpwiedź zawiera adres logiczny i fizyczny hosta docelowego
+- odebrany adres fizyczny zapisywany jest w tablicy ARP i parowany z
+adresem logicznym hosta docelowego, dzięki czemu nie będzie
+wymagane ponowne odkrywanie adresu fizycznego do momentu wyczyszczenia
+tablicy
+"""
