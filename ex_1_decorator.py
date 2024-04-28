0680d51c4f033044e72ba4c6df4f4eabf892dc40
SzymKam <szymon15kaminski@gmail.com>
2024-04-28 17:02:37 +0200

    fix: update exercise

--- ex_1_decorator.py
+++ ex_1_decorator.py
@@ -1,40 +1,40 @@
-"""
-Napisz program w pythonie, który zrobi łańcuch decoratorów, i udekoruje podany text tak jak w przypadku html na BOLD, ITALIC, UNDERLINE
-
-Input :
-
-Hello world
-
-Output:
-
-<b><i><u>hello world</u></i></b>
-
-"""
-
-
-def add_bolt(func):
-    def inner(*args, **kwargs):
-        return f"<b>{func(*args, **kwargs)}</b>"
-    return inner
-
-
-def add_italic(func):
-    def inner(*args, **kwargs):
-        return f"<i>{func(*args, **kwargs)}</i>"
-    return inner
-
-
-def add_underline(func):
-    def inner(*args, **kwargs):
-        return f"<u>{func(*args, **kwargs)}</u>"
-    return inner
-
-
-@add_underline
-@add_italic
-@add_bolt
-def hello(text: str):
-    return text
-
-
-print(hello("Hello"))
+# """
+# Napisz program w pythonie, który zrobi łańcuch decoratorów, i udekoruje podany text tak jak w przypadku html na BOLD, ITALIC, UNDERLINE
+#
+# Input :
+#
+# Hello world
+#
+# Output:
+#
+# <b><i><u>hello world</u></i></b>
+#
+# """
+#
+#
+# def add_bolt(func):
+#     def inner(*args, **kwargs):
+#         return f"<b>{func(*args, **kwargs)}</b>"
+#     return inner
+#
+#
+# def add_italic(func):
+#     def inner(*args, **kwargs):
+#         return f"<i>{func(*args, **kwargs)}</i>"
+#     return inner
+#
+#
+# def add_underline(func):
+#     def inner(*args, **kwargs):
+#         return f"<u>{func(*args, **kwargs)}</u>"
+#     return inner
+#
+#
+# @add_underline
+# @add_italic
+# @add_bolt
+# def hello(text: str):
+#     return text
+#
+#
+# print(hello("Hello"))
