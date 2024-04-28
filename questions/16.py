949cb7c623614712a89a4e6e4378caed7470811d
SzymKam <szymon15kaminski@gmail.com>
2024-04-28 17:57:18 +0200

    fix: update exercise

--- questions/16.py
+++ questions/16.py
@@ -19,4 +19,5 @@ UDP - nie daje takiej gwarancji, ale szybsze
 
 TCP gdy ważna jest niezawodność, np. przesył plików, stron.
 UDP - gdy potrzeba szybkości np. gry online, transmije strumieniowe
+
 """
