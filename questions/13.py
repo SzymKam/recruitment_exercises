979864319e713af31b46cdf9f35ce8d01592cc01
SzymKam <szymon15kaminski@gmail.com>
2024-04-29 22:54:49 +0200

    fix: update exercise

--- questions/13.py
+++ questions/13.py
@@ -7,5 +7,4 @@ routowane w Internecie. Są to zakresy:
 * 10.0.0.0 - 10.255.255.255 (10.0.0.0/8) - czyli adresy zaczynające się od 10.xx.xx.xx - sieci korporacyje i domowe
 * 172.16.0.0 - 172.31.255.255 (172.16.0.0/12) - adresy z zakresu 172.16-31.xx.xx - korporacyjne
 * 192.168.0.0 - 192.168.255.255 (192.168.0.0/16) - adresy 192.168.xx.xx - sieci domowem małe firmy i prywatne środowiska
-
 """
